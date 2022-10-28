17ca5d50f1fd3d0a3a108d25c7869b59211b2699
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-28 09:11:21 +0600

    web font added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import Header from './component/layout/Header/Header'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import Header from "./component/layout/Header/Header";
+import { BrowserRouter as Router } from "react-router-dom";
+import WebFont from "webfontloader";
+
+
 export default function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto" ]
+      },
+    });
+  }, []);
   return (
     <Router>
-        <Header/>
-        <h2>u</h2>
+      <Header />
     </Router>
-  
-  )
+  );
 }
