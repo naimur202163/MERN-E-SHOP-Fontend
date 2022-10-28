c426dbd4a8fb8dd2b49b31d517b82b0849f51ebf
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-28 08:45:41 +0600

    logo added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import Header from './component/layout/Header/Header'
+import { BrowserRouter as Router } from 'react-router-dom'
 export default function App() {
   return (
-    <div>Hello</div>
+    <Router>
+        <Header/>
+        <h2>u</h2>
+    </Router>
+  
   )
 }
