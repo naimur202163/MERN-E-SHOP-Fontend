9d9f4a27ff6ba7f42ff19688f15bccea31988c45
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-30 02:06:45 +0600

    productCard added

--- src/component/Product/ProductDetails.js
+++ src/component/Product/ProductDetails.js
@@ -1,17 +1,34 @@
 import React, { Fragment, useEffect } from "react";
 import Carousel from "react-material-ui-carousel";
 import { useSelector, useDispatch } from "react-redux";
-import { getProductDetails } from "../../actions/productAction";
+import { clearErrors, getProductDetails } from "../../actions/productAction";
 import "./ProductDetails.css";
 import { Rating } from "@material-ui/lab";
-import MetaData from './../layout/MetaData';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@material-ui/core";
+import MetaData from "./../layout/MetaData";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@material-ui/core";
 import Loader from "../layout/Loader/Loader";
+import ReviewCard from "./ReviewCard";
+import { useAlert } from "react-alert";
 const ProductDetails = ({ match }) => {
+  const alert = useAlert();
+
   const dispatch = useDispatch();
-  const { product, error,loading } = useSelector((state) => state.productDetails);
+  const { product, error, loading } = useSelector(
+    (state) => state.productDetails
+  );
 
   useEffect(() => {
+    if (error) {
+       alert.error(error);
+       dispatch(clearErrors())
+    }
+
     dispatch(getProductDetails(match.params.id));
   }, [dispatch, match.params.id, error]);
 
@@ -24,113 +41,99 @@ const ProductDetails = ({ match }) => {
 
   return (
     <Fragment>
-    {loading ? (
-      <Loader />
-    ) : (
-      <Fragment>
-        <MetaData title={`${product.name} -- ECOMMERCE`} />
-        <div className="ProductDetails">
-          <div>
-            <Carousel>
-              {product.images &&
-                product.images.map((item, i) => (
-                  <img
-                    className="CarouselImage"
-                    key={i}
-                    src={item.url}
-                    alt={`${i} Slide`}
-                  />
-                ))}
-            </Carousel>
-          </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title={`${product.name} -- ECOMMERCE`} />
+          <div className="ProductDetails">
+            <div>
+              <Carousel>
+                {product.images &&
+                  product.images.map((item, i) => (
+                    <img
+                      className="CarouselImage"
+                      key={i}
+                      src={item.url}
+                      alt={`${i} Slide`}
+                    />
+                  ))}
+              </Carousel>
+            </div>
 
-          <div>
-            <div className="detailsBlock-1">
-              <h2>{product.name}</h2>
-              <p>Product # {product._id}</p>
-            </div>
-            <div className="detailsBlock-2">
-              <Rating {...options} />
-              <span className="detailsBlock-2-span">
-                {" "}
-                ({product.numOfReviews} Reviews)
-              </span>
-            </div>
-            <div className="detailsBlock-3">
-              <h1>{`₹${product.price}`}</h1>
-              <div className="detailsBlock-3-1">
-                <div className="detailsBlock-3-1-1">
-                  <button >-</button>
-                  <input readOnly type="number"  />
-                  <button>+</button>
+            <div>
+              <div className="detailsBlock-1">
+                <h2>{product.name}</h2>
+                <p>Product # {product._id}</p>
+              </div>
+              <div className="detailsBlock-2">
+                <Rating {...options} />
+                <span className="detailsBlock-2-span">
+                  {" "}
+                  ({product.numOfReviews} Reviews)
+                </span>
+              </div>
+              <div className="detailsBlock-3">
+                <h1>{`₹${product.price}`}</h1>
+                <div className="detailsBlock-3-1">
+                  <div className="detailsBlock-3-1-1">
+                    <button>-</button>
+                    <input readOnly type="number" />
+                    <button>+</button>
+                  </div>
+                  <button disabled={product.Stock < 1 ? true : false}>
+                    Add to Cart
+                  </button>
                 </div>
-                <button
-                  disabled={product.Stock < 1 ? true : false}
-                 
-                >
-                  Add to Cart
-                </button>
+
+                <p>
+                  Status:
+                  <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                    {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                  </b>
+                </p>
               </div>
 
-              <p>
-                Status:
-                <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
-                  {product.Stock < 1 ? "OutOfStock" : "InStock"}
-                </b>
-              </p>
+              <div className="detailsBlock-4">
+                Description : <p>{product.description}</p>
+              </div>
+
+              <button className="submitReview">Submit Review</button>
             </div>
+          </div>
 
-            <div className="detailsBlock-4">
-              Description : <p>{product.description}</p>
+          <h3 className="reviewsHeading">REVIEWS</h3>
+
+          <Dialog aria-labelledby="simple-dialog-title">
+            <DialogTitle>Submit Review</DialogTitle>
+            <DialogContent className="submitDialog">
+              <Rating size="large" />
+
+              <textarea
+                className="submitDialogTextArea"
+                cols="30"
+                rows="5"
+              ></textarea>
+            </DialogContent>
+            <DialogActions>
+              <Button color="secondary">Cancel</Button>
+              <Button color="primary">Submit</Button>
+            </DialogActions>
+          </Dialog>
+
+          {product.reviews && product.reviews[0] ? (
+            <div className="reviews">
+              {product.reviews &&
+                product.reviews.map((review) => (
+                  <ReviewCard key={review._id} review={review} />
+                ))}
             </div>
-
-            <button  className="submitReview">
-              Submit Review
-            </button>
-          </div>
-        </div>
-
-        <h3 className="reviewsHeading">REVIEWS</h3>
-
-        <Dialog
-          aria-labelledby="simple-dialog-title"
-         
-        >
-          <DialogTitle>Submit Review</DialogTitle>
-          <DialogContent className="submitDialog">
-            <Rating
-              size="large"
-            />
-
-            <textarea
-              className="submitDialogTextArea"
-              cols="30"
-              rows="5"
-            ></textarea>
-          </DialogContent>
-          <DialogActions>
-            <Button color="secondary">
-              Cancel
-            </Button>
-            <Button  color="primary">
-              Submit
-            </Button>
-          </DialogActions>
-        </Dialog>
-
-        {/* {product.reviews && product.reviews[0] ? (
-          <div className="reviews">
-            {product.reviews &&
-              product.reviews.map((review) => (
-                <ReviewCard key={review._id} review={review} />
-              ))}
-          </div>
-        ) : (
-          <p className="noReviews">No Reviews Yet</p>
-        )} */}
-      </Fragment>
-    )}
-  </Fragment>
+          ) : (
+            <p className="noReviews">No Reviews Yet</p>
+          )}
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
--- src/component/Product/ReviewCard.js
+++ src/component/Product/ReviewCard.js
@@ -1,7 +1,18 @@
-import React from 'react'
-
-export default function ReviewCard() {
+import React from "react";
+import ReactStars from "react-stars";
+import profilePng from "../../images/Profile.png";
+export default function ReviewCard({ review }) {
+  const options = {
+    value: review.rating,
+    readOnly: true,
+    precision: 0.5,
+  };
   return (
-    <div>ReviewCard</div>
-  )
+    <div>
+      <img src={profilePng} alt="User" />
+      <p>{review.name}</p>
+      <ReactStars {...options} />
+      <span>{review.comment}</span>
+    </div>
+  );
 }
