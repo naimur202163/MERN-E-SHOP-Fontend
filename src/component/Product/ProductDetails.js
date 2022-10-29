86bc9c3f71a3e31281e42431137a2c43fe988923
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-30 01:32:24 +0600

    product details added

--- src/component/Product/ProductDetails.js
+++ src/component/Product/ProductDetails.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getProductDetails } from "../../actions/productAction";
 import "./ProductDetails.css";
 import { Rating } from "@material-ui/lab";
+import MetaData from './../layout/MetaData';
 const ProductDetails = ({ match }) => {
   const dispatch = useDispatch();
   const { product, error } = useSelector((state) => state.productDetails);
@@ -12,42 +13,75 @@ const ProductDetails = ({ match }) => {
     dispatch(getProductDetails(match.params.id));
   }, [dispatch, match.params.id, error]);
 
-  return (
-    <Fragment>
-      <div className="ProductDetails">
-        <div>
-          <Carousel>
-            {product.images &&
-              product.images.map((item, i) => (
-                <img
-                  className="CarouselImage"
-                  key={i}
-                  src={item.url}
-                  alt={`${i} Slide`}
-                />
-              ))}
-          </Carousel>
-        </div>
-        <div className="detailsBlock-1">
-          <h2>{product.name}</h2>
-          <p>Product # {product._id}</p>
-        </div>
-        <div className="detailsBlock-2">
-          <Rating />
-          <span>{product.numOfReviews} Reviews</span>
-        </div>
-        <div className="detailsBlock-3">
-          <h1>{`${product.price}`}</h1>
+  const options = {
+    size: "large",
+    value: product.ratings,
+    readOnly: true,
+    precision: 0.5,
+  };
 
-          <div className="detailsBlock-1">
-            <div className="detailsBlock-1-1">
-              <button>-</button>
-              <input value="1" type="number" />
-              <button>+</button>
+  return (
+      <Fragment>
+          <MetaData title={`${product.name} -- ECOMMERCE`} />
+          <div className="ProductDetails">
+            <div>
+              <Carousel>
+                {product.images &&
+                  product.images.map((item, i) => (
+                    <img
+                      className="CarouselImage"
+                      key={i}
+                      src={item.url}
+                      alt={`${i} Slide`}
+                    />
+                  ))}
+              </Carousel>
+            </div>
+
+            <div>
+              <div className="detailsBlock-1">
+                <h2>{product.name}</h2>
+                <p>Product # {product._id}</p>
+              </div>
+              <div className="detailsBlock-2">
+                <Rating {...options} />
+                <span className="detailsBlock-2-span">
+                  {" "}
+                  ({product.numOfReviews} Reviews)
+                </span>
+              </div>
+              <div className="detailsBlock-3">
+                <h1>{`₹${product.price}`}</h1>
+                <div className="detailsBlock-3-1">
+                  <div className="detailsBlock-3-1-1">
+                    <button>-</button>
+                    <input readOnly type="number"  />
+                    <button >+</button>
+                  </div>
+                  <button
+                   
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+
+                <p>
+                  Status:
+                  <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                    {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                  </b>
+                </p>
+              </div>
+
+              <div className="detailsBlock-4">
+                Description : <p>{product.description}</p>
+              </div>
+
+              <button className="submitReview">
+                Submit Review
+              </button>
             </div>
           </div>
-        </div>
-      </div>
     </Fragment>
   );
 };
