a532898a588a05738f5d3384cd547fbe38ae454b
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-29 14:07:01 +0600

    proxy server problem solved

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -6,43 +6,47 @@ import MetaData from "../layout/MetaData";
 import { useDispatch } from "react-redux";
 import { getProduct } from "./../../actions/productAction";
 import { useSelector } from "react-redux";
+import Loader from "./../layout/Loader/Loader";
+import { useAlert } from "react-alert";
 
 export default function Home() {
+  const alert = useAlert();
   const dispatch = useDispatch();
   const { loading, error, products, productsCount } = useSelector(
     (state) => state.products
   );
 
-  // const product = {
-  //   edit: false,
-  //   isHalf: true,
-  //   name: "Blue  Tshirt",
-  //   images: [{ url: "https://i.ibb.co/hDysN82/top1.jpg" }],
-  //   price: "$300",
-  //   _id: "naimur",
-  //   size: window.innerWidth < 600 ? 20 : 25,
-  // };
   React.useEffect(() => {
+    if (error) {
+      return alert.error(error);
+    }
     dispatch(getProduct);
-  }, [dispatch]);
+  }, [dispatch, error, alert]);
   // console.log(getProduct)
   return (
     <Fragment>
-      <MetaData title="Ecommerce" />
-      <div className="banner">
-        <p>Wellcome to Ecommerce</p>
-        <h1>FIND AMAZING PRODUCTS BELOW</h1>
-        <a href="#container">
-          <button>
-            Scroll
-            <CgMouse />
-          </button>
-        </a>
-      </div>
-      <h2 className="homeHeading">Featured Products</h2>
-      <div className="container" id="container">
-        {products && products.map((product) => <Product product={product} />)}
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title="Ecommerce" />
+          <div className="banner">
+            <p>Wellcome to Ecommerce</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+            <a href="#container">
+              <button>
+                Scroll
+                <CgMouse />
+              </button>
+            </a>
+          </div>
+          <h2 className="homeHeading">Featured Products</h2>
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => <Product product={product} />)}
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./store";
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+const options={
+  timeout:5000,
+  positions:positions.BOTTOM_CENTER,
+  transitions:transitions.SCALE,
+}
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
   </Provider>,
 
   document.getElementById("root")
