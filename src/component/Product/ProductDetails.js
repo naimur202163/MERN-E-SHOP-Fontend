222e3f3461a597f7347e9c6db5cc463b184e702d
MD Naimur Rahman <86639464+naimur202163@users.noreply.github.com>
2022-10-29 15:15:21 +0600

    product details success added

--- src/component/Product/ProductDetails.js
+++ src/component/Product/ProductDetails.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Fragment } from "react";
+import Carousel from "react-material-ui-carousel";
+import "./ProductDetails.css";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { getProductDetails } from "./../../actions/productAction";
 
-export default function ProductDetails() {
+export default function ProductDetails({ match }) {
+  const dispatch = useDispatch();
+  const { product, loading, error } = useSelector(
+    (state) => state.productDetails
+  );
+
+  React.useEffect(() => {
+    dispatch(getProductDetails(match.params.id));
+  }, [dispatch, match.params.id]);
+
   return (
-    <div>ProductDetails</div>
-  )
+    <Fragment>
+      <div className="productDetails">
+        <div>
+          {/* <Carousel>
+            {product.images &&
+              product.images.map((item, i) => (
+                <img
+                  className="CarouselImage"
+                  key={item.url}
+                  src={item.url}
+                  alt={`${i} Slide`}
+                />
+              ))}
+          </Carousel> */}
+
+          <h2>Product Details</h2>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
